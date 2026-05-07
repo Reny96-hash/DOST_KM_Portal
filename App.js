869065768a2cbf4935e555b87759c2b100ac7bfa
Reny Ann Cherian <reny.ann.96cherian@gmail.com>
2026-05-07 18:14:24 +1000

    Revert "Changes made to Upload files and User roles"
    This reverts commit 4ff7bd3afd0a2b58cf121e137a3c7eed4fc4eacb.

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './src/contexts/AuthContext';
-import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AuthProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/services/topicService.js
+++
@@ -1,9 +0,0 @@
-import { apiRequest } from './api';
-
-export const topicService = {
-  getTopics: () => apiRequest('/topics'),
-  getTopic: (id) => apiRequest(`/topics/${id}`),
-  createTopic: (data) => apiRequest('/topics', { method: 'POST', body: JSON.stringify(data) }),
-  updateTopic: (id, data) => apiRequest(`/topics/${id}`, { method: 'PUT', body: JSON.stringify(data) }),
-  deleteTopic: (id) => apiRequest(`/topics/${id}`, { method: 'DELETE' }),
-};
